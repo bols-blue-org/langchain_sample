799df9d8cb6e4b1fb61ec3b88cbdfb5c9b179325
hisayoshi.suehiro <hisayoshi.suehiro@access-company.com>
2023-07-18 11:29:52 +0900

    テストコードの追加 若干のリファクタリング

--- langchain_sample/minutes/loader.py
+++ langchain_sample/minutes/loader.py
@@ -1,11 +1,13 @@
 import json
+import logging
 import os
 
 import tiktoken
-from notion_client import Client
+from notion_client import Client, APIResponseError, APIErrorCode
 
 # ディレクトリ内のすべてのファイルに対して処理をするためのクラスを作成するユーティリティ関数
 from langchain_sample.langchain_minutes import create_abstruct, create_action_item
+from langchain_sample.send_notion import sprit_limit_len
 
 
 def file_loader(path):
@@ -22,10 +24,10 @@ class FilePath:
         self.directory, self.filename = os.path.split(path)  # パスをディレクトリとファイル名に分割
         self.name, self.extension = os.path.splitext(self.filename)  # ファイル名を名称と拡張子に分割
 
-    def save_to_json(self, file_path):
+    def save_to_json(self, dir_path):
         data = self.__dict__  # クラスのインスタンスの属性を辞書として取得
-        with open(file_path, 'w') as file:
-            json.dump(data, file)
+        with open(os.path.join(dir_path, self.name+".json"), 'w') as file:
+            json.dump(data, file, ensure_ascii=False)
 
     @classmethod
     def load_from_json(cls, file_path):
@@ -37,8 +39,6 @@ class FilePath:
 
 
 class Minutes(FilePath):
-    abstruct = []
-    action_item = []
 
     def create_abstruct(self):
         if len(self.abstruct) == 0:
@@ -73,12 +73,17 @@ class Minutes(FilePath):
         with open(self.absolute_path, 'r', encoding='utf-8') as file:
             self.content = file.read()
             self.split_from_token = self.contents_split_limit_tokens(self.content)
-            split_from_token = self.contents_split_limit_tokens(self.content)
+            self.abstruct = []
+            self.action_item = []
 
 
 class NotionController:
+    pages = ""
+    paragraph = {"paragraph": {"rich_text": [{"text": {"content": "I'm a paragraph."}}]}}
 
-    def __init__(self):
+
+    def __init__(self, database_id):
+        self.database_id = database_id
         self.notion = Client(auth=os.environ["NOTION_TOKEN"])
         pass
 
@@ -86,3 +91,110 @@ class NotionController:
         response = self.notion.databases.list()
         for database in response.results:
             print(database.title)
+
+    def load_page_contents(self, database_id=None):
+        if database_id is None:
+            database_id = self.database_id
+        try:
+            self.pages = self.notion.databases.query(
+                **{
+                    "database_id": database_id
+                }
+            )
+        except APIResponseError as error:
+            if error.code == APIErrorCode.ObjectNotFound:
+                ...  # For example: handle by asking the user to select a different database
+            else:
+                # Other error handling code
+                logging.error(error)
+
+    def create_database_page(self, database_id=None):
+        if database_id is None:
+            database_id = self.database_id
+        try:
+            response = self.notion.pages.create(
+                parent={"database_id": database_id},
+                properties={
+                    "title": [{"text": {"content": "Test Page"}}],
+                },
+                icon={"type": "emoji", "emoji": "🤠"},
+                children=[self.paragraph],
+            )
+        except APIResponseError as error:
+            if error.code == APIErrorCode.ObjectNotFound:
+                ...  # For example: handle by asking the user to select a different database
+            else:
+                # Other error handling code
+                logging.error(error)
+
+    def create_auto_minutes(self, minutes:Minutes,  database_id=None):
+        minutes.name
+        children = []
+        children.extend(self.split_topic_text(minutes))
+        children.extend(self.split_raw_text(minutes.content))
+        if database_id is None:
+            database_id = self.database_id
+        try:
+            response = self.notion.pages.create(
+                parent={"database_id": database_id},
+                properties={
+                    "title": [{"text": {"content": minutes.name}}],
+                },
+                icon={"type": "emoji", "emoji": "🤠"},
+                children=children,
+            )
+        except APIResponseError as error:
+            if error.code == APIErrorCode.ObjectNotFound:
+                ...  # For example: handle by asking the user to select a different database
+            else:
+                # Other error handling code
+                logging.error(error)
+
+    def create_paragraph(self, text):
+        return {
+                "object": 'block',
+                "type": 'paragraph',
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "text": {
+                                "content": text
+                            }
+                        }
+                    ],
+                }
+            }
+
+    def create_topic(self, text):
+        return {
+                "object": 'block',
+                "type": 'heading_1',
+                "heading_1": {
+                    "rich_text": [
+                        {
+                            "text": {
+                                "content": text
+                            }
+                        }
+                    ],
+                }
+            }
+
+    def split_raw_text(self, content):
+        splited_text = sprit_limit_len(content)
+        ret = []
+        for tmp in splited_text:
+            ret.append(self.create_paragraph(tmp))
+        return ret
+
+    def split_topic_text(self, minutes:Minutes):
+        ret = [self.create_topic("Topics!!!")]
+        for tmp in minutes.abstruct:
+            ret.append(self.create_topic(tmp))
+        return ret
+
+    def split_action_item_text(self, minutes:Minutes):
+        ret = [self.create_("Todo")]
+        for tmp in minutes.action_item:
+            ret.append(self.create_paragraph(tmp))
+        return ret
--- tests/minutes_test.py
+++ tests/minutes_test.py
@@ -4,7 +4,7 @@ import unittest
 from langchain_sample.langchain_minutes import create_abstruct, create_action_item
 import tiktoken
 
-from langchain_sample.minutes.loader import Minutes
+from langchain_sample.minutes.loader import Minutes, NotionController
 from langchain_sample.send_notion import create_meeting_log, parse_file_path, get_meeting_log
 
 
@@ -13,21 +13,27 @@ class MyTestCase(unittest.TestCase):
         path ="resoces/"
         files = os.listdir(path)
         for name in files:
-            print(name)
-            print(parse_file_path(name))
             ret = Minutes(path + "/" + name)
             ret.create_abstruct()
             ret.create_action_item()
-            print(ret.abstruct)
+            print("file path:" + ret.absolute_path+"\t" + str(ret.abstruct))
             print(ret.action_item)
-            create_meeting_log(name, ret.content)
+            database_id = "3b6673eca2414b81832904f837497fc5"
+            notion = NotionController(database_id)
+            notion.create_auto_minutes(ret)
+            ret.save_to_json("./")
         self.assertEqual(True, True)  # add assertion here
 
+    # データベースの読み込みテスト
+    def test_database(self):
+        database_id ="3b6673eca2414b81832904f837497fc5"
+        notion = NotionController(database_id)
+        notion.load_page_contents()
+        notion.create_database_page()
 
     def test_send_notion(self):
         get_meeting_log()
         create_meeting_log("テスト送信", "テスト", mocking=True)
-        self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
